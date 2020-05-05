772a7d8b629f2ab414e2751806fc9ae8ffc133e7
jeason58 <2356914868@qq.com>
2020-05-05 22:07:23 +0800

    SimplifyPath添加注释

--- algorithm/string/path_test.go
+++ algorithm/string/path_test.go
@@ -1,7 +1,21 @@
 package string
 
-import "testing"
+import (
+	"runtime"
+	"runtime/debug"
+	"testing"
+)
 
 func TestSimplifyPath(t *testing.T) {
 
+	//设置"M"最大数量（内核线程的最大数量）
+	debug.SetMaxThreads(10000)
+
+	//设置"P"的最大数量（G运行的上下文环境P）
+	runtime.GOMAXPROCS(runtime.NumCPU())
+
+	go func() {
+		//将当前G与当前M（核心线程）绑定
+		runtime.LockOSThread()
+	}()
 }
